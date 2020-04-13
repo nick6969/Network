6a6cbd5f63e444fc9240cd4d05445f38c198c2f5
nick <hawhaw.ya@gmail.com>
2020-04-13 09:07:20 +0800

    add request demo - AppleStoreAppInfo

--- Network/ViewController.swift
+++ Network/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        sendRequest()
     }
 
+    private
+    func sendRequest() {
+        let req = AppleStoreAppInfo(identifier: "1069511488", country: "tw")
+        let plugins = [Logger()]
+        HTTPClient(session: URLSession.shared).send(req, plugins: plugins) { result in
+            switch result {
+            case .success(let model):
+                print(model.results.first?.trackCensoredName ?? "EMPTY title")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
